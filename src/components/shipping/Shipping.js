b0abdb215df21654c87783060888a9d0720da3cc
Barry Rollan <barryrollan@macbook-pro.lan>
2020-06-06 10:24:38 -0400

    finished shipping page

--- src/components/shipping/Shipping.js
+++ src/components/shipping/Shipping.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Col, Row, Button } from 'react-bootstrap';
 import FedExMap from '../../media/fedExMap.png';
 import styles from './Shipping.module.scss';
 
 const Shipping = () => {
+  const [showExample, setShowExample] = useState(false);
+
   return (
     <div id="shipping" className={styles.shipping}>
       <h3 style={{ textDecoration: 'underline' }}>
@@ -20,9 +22,9 @@ const Shipping = () => {
         estimated shipping rate when we receive your order.
       </h6>
       <img src={FedExMap} alt="" className={styles.mapImg} />
-      <Container style={{ margin: '3vh 0' }}>
+      <Container style={{ margin: '3vh auto' }}>
         <h4 style={{ textDecoration: 'underline' }}>
-          Total Shipping charges are base Costs plus Fuel Surcharge
+          Total Shipping charges are Base Costs plus Fuel Surcharge
         </h4>
         <Container
           className={styles.outlined}
@@ -32,24 +34,96 @@ const Shipping = () => {
             Boxes, Packaging &amp; Shipping Base Costs
           </h5>
           <Row style={{ borderTop: '1px solid var(--normal-font-color)' }}>
-            <Col className={styles.outlined}>$0.00 to $10.00</Col>
-            <Col className={styles.outlined}>$10.01 to $25.00</Col>
-            <Col className={styles.outlined}>$25.01 to $50.00</Col>
-            <Col className={styles.outlined}>$50.01 to $75.00</Col>
-            <Col className={styles.outlined}>$75.01 to $100.00</Col>
-            <Col className={styles.outlined}>over $100.00</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $0.00 to $10.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $10.01 to $25.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $25.01 to $50.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $50.01 to $75.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $75.01 to $100.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              over $100.00
+            </Col>
           </Row>
           <Row>
-            <Col className={styles.outlined}>$15.00</Col>
-            <Col className={styles.outlined}>$17.00</Col>
-            <Col className={styles.outlined}>$19.00</Col>
-            <Col className={styles.outlined}>$21.00</Col>
-            <Col className={styles.outlined}>$23.00</Col>
-            <Col className={styles.outlined}>28%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $15.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $17.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $19.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $21.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              $23.00
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              28%
+            </Col>
           </Row>
         </Container>
         <Container
-          className={styles.outlined}
+          className={[`${styles.outlined} ${styles.responsiveFont}`]}
           style={{
             borderTop: '2px solid var(--normal-font-color)',
             margin: '3vh 0',
@@ -58,66 +132,223 @@ const Shipping = () => {
           <h5 style={{ paddingTop: '5px' }}>Fuel Surcharge Costs</h5>
           <Row style={{ borderTop: '1px solid var(--normal-font-color)' }}>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#EB357F' }}
             >
               Zone 1
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>0%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              0%
+            </Col>
           </Row>{' '}
           <Row>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#73FBFD' }}
             >
               Zone 2
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>10%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              10%
+            </Col>
           </Row>{' '}
           <Row>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#EF8633' }}
             >
               Zone 3
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>20%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              20%
+            </Col>
           </Row>{' '}
           <Row>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#75F94C' }}
             >
               Zone 4
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>30%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              30%
+            </Col>
           </Row>{' '}
           <Row>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#461F86' }}
             >
               Zone 5
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>40%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              40%
+            </Col>
           </Row>{' '}
           <Row>
             <Col
-              className={styles.outlined}
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
               style={{ backgroundColor: '#0023F5' }}
             >
               Zone 6
             </Col>
-            <Col className={styles.outlined}>Base Shipping + </Col>
-            <Col className={styles.outlined}>50%</Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              Base Shipping +{' '}
+            </Col>
+            <Col
+              className={[
+                `${styles.outlined} ${styles.responsiveFont} ${styles.flexCenter}`,
+              ]}
+            >
+              50%
+            </Col>
           </Row>
         </Container>
       </Container>
+      <Button
+        className={styles.showExample}
+        onClick={() => setShowExample(true)}
+        style={{ display: showExample ? 'none' : 'initial' }}
+      >
+        See Cost Example
+      </Button>
+      <Container
+        className={styles.outlined}
+        style={{
+          borderTop: '2px solid var(--normal-font-color)',
+          margin: '3vh 5%',
+          width: '90%',
+          padding: '1vw',
+          display: showExample ? 'inline-block' : 'none',
+        }}
+      >
+        <h5
+          style={{
+            textDecoration: 'underline',
+          }}
+        >
+          Shipping Cost Example:
+        </h5>
+        <div
+          className={[
+            `${styles.responsiveFont} ${styles.flexSpaceBetween} ${styles.math}`,
+          ]}
+        >
+          <span className={styles.responsiveFont}>Merchandise: </span>
+          <span className={styles.responsiveFont}>$37.00</span>
+        </div>
+        <div
+          className={[
+            `${styles.responsiveFont} ${styles.flexSpaceBetween} ${styles.math}`,
+          ]}
+        >
+          <span className={styles.responsiveFont}>Base shipping cost: </span>
+          <span className={styles.responsiveFont}>$19.00</span>
+        </div>
+        <div
+          className={[
+            `${styles.responsiveFont} ${styles.flexSpaceBetween} ${styles.math}`,
+          ]}
+        >
+          <span className={styles.responsiveFont}>
+            Surcharge delivery to Zone 3 &nbsp;($19 * 20%):{' '}
+          </span>
+          <span
+            className={styles.responsiveFont}
+            style={{ borderBottom: '1px solid var(--normal-font-color)' }}
+          >
+            $3.80
+          </span>
+        </div>
+        <div
+          className={[
+            `${styles.responsiveFont} ${styles.flexSpaceBetween} ${styles.math}`,
+          ]}
+        >
+          <span className={styles.responsiveFont}>
+            Total cost of order including shipping and fuel surcharge ={' '}
+          </span>
+          <span className={styles.responsiveFont}>$59.80</span>
+        </div>
+      </Container>
+      <Button
+        className={styles.showExample}
+        onClick={() => setShowExample(false)}
+        style={{ display: showExample ? 'inline-block' : 'none' }}
+      >
+        Close Cost Example
+      </Button>
     </div>
   );
 };
